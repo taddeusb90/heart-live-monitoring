1e6b63e9adee3c3a834b6071aa910ad974458623
Taddeus Buica <taddeus_b90@hotmail.com>
2025-03-15 16:57:05 +0200

    init repo

---
+++ main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, 'dist/heart-live-monitoring/index.html'),
+      protocol: 'file:',
+      slashes: true,
+    })
+  );
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'electron-app' title`, () => {
+  it(`should have the 'heart-live-monitoring' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('electron-app');
+    expect(app.title).toEqual('heart-live-monitoring');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, electron-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, heart-live-monitoring');
   });
 });
